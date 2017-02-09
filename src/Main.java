c945719e27b6086af7c8cdc485b2df2b1586a128
Julie Schneider <julieschneider@Julies-MacBook-Pro.local>
2017-02-09 16:49:23 -0500

    lab10

--- src/Main.java
+++ src/Main.java
@@ -14,12 +14,7 @@ public class Main {
         System.out.println();
         ArrayList<Car> carList = new ArrayList<Car>();
 
-        carList.add(new Car("Ford", "Explorer", 2016, 27999.99));
-        carList.add(new Car("Chevrolet", "Cruze", 2017, 21999.99));
-        carList.add(new Car("Buick", "Envision", 2017, 60999.99));
-        carList.add(new UsedCar("Chevrolet", "Silverado", 2000, 10899.99, 70000));
-        carList.add(new UsedCar("Ford", "Escape", 2007, 12890.99, 50000));
-        carList.add(new UsedCar("Chevrolet", "Corvette", 2005, 15999.99, 12000));
+        listOfCars(carList);
 
         do {
 
@@ -27,32 +22,16 @@ public class Main {
             System.out.println();
 
             System.out.println("Which car would you like?");
-            carPick = scan.nextInt();
-            scan.nextLine();
+            carPick = rangeValidator();
 
             System.out.println(carList.get(carPick - 1));
+            //extracts the line the user selected
             System.out.println();
 
             System.out.println("Would you like to buy this car? y/n");
             userInput = scan.nextLine();
 
-            if (userInput.equalsIgnoreCase("y")) {
-
-                System.out.println("Excellent! Our finance department will be in touch shortly.");
-                System.out.println();
-                //System.out.println();
-                carList.remove(carPick - 1);
-                displayCars(carList);
-                System.out.println();
-
-            } else {
-
-                System.out.println();
-                System.out.println("Fine. We didn't want you to buy a car anyway.");
-                System.out.println("Buh-Bye now.");
-                System.out.println();
-
-            }
+            askToBuyCar(carPick, userInput, carList);
 
             System.out.println("Do you want to continue? y/n");
             userInput = scan.nextLine();
@@ -63,12 +42,67 @@ public class Main {
 
     }
 
+    public static void listOfCars(ArrayList<Car> carList) {
+
+        carList.add(new Car("Ford", "Explorer", 2016, 27999.99));
+        carList.add(new Car("Chevrolet", "Cruze", 2017, 21999.99));
+        carList.add(new Car("Buick", "Envision", 2017, 60999.99));
+        carList.add(new UsedCar("Chevrolet", "Silverado", 2000, 10899.99, 70000));
+        carList.add(new UsedCar("Ford", "Escape", 2007, 12890.99, 50000));
+        carList.add(new UsedCar("Chevrolet", "Corvette", 2005, 15999.99, 12000));
+
+    }
+
+    public static void askToBuyCar(int carPick, String userInput, ArrayList<Car> carList) {
+
+        if (userInput.equalsIgnoreCase("y")) {
+
+            System.out.println("Excellent! Our finance department will be in touch shortly.");
+            System.out.println();
+            carList.remove(carPick - 1);
+            //removes the car that the user selected yes to purchase
+            displayCars(carList);
+            System.out.println();
+
+        } else {
+
+            System.out.println();
+            System.out.println("Fine. We didn't want you to buy a car anyway.");
+            System.out.println("Buh-Bye now.");
+            System.out.println();
+
+        }
+
+        //above if else statement determines if user wants to buy car, then asks if they want to continue
+    }
+
     public static void displayCars(ArrayList<Car> carList) {
 
         for (int i = 0; i < carList.size(); i++) {
             System.out.printf("%d. %s\n", i + 1, carList.get(i).toString());
         }
 
+        //formats and displays the number of cars, along with a description of each
+        //%d. creates a . next to each #, i + 1 makes the numbers get higher along with the list
+
     }
 
+    public static int rangeValidator() {
+
+        Scanner scan = new Scanner(System.in);
+
+        int input = scan.nextInt();
+
+        while (input < 1 || input > 6) {
+
+            System.out.println("please enter a number between 1 and 6!");
+            input = scan.nextInt();
+            scan.nextLine();
+        }
+
+        return input;
+
+        //validates that the user is inputting a valid number
+
+    }
 }
